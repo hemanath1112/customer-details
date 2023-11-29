b09c148d975804b95d8cf98cf9a276a207c1756d
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-11-29 12:49:34 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import {
   SafeAreaView,
   StatusBar,
@@ -9,10 +9,10 @@ import {
   FlatList,
   RefreshControl,
   ActivityIndicator,
-} from 'react-native';
+} from "react-native";
 
-import Child from './Component/Child';
-import api from './Api/api';
+import Child from "./Component/Child";
+import api from "./Api/api";
 
 function App(): JSX.Element {
   const [data, setData] = useState([]);
@@ -26,14 +26,14 @@ function App(): JSX.Element {
   const fetchData = () => {
     axios
       .get(`https://randomuser.me/api/?page=${curentPage}&results=20`)
-      .then(response => {
+      .then((response) => {
         setTimeout(() => {
           setData([...data, ...response.data.results]);
           setRefreshing(false);
         }, 3000);
       })
 
-      .catch(err => console.log(err))
+      .catch((err) => console.log(err))
       .finally(() => {
         setRefreshing(false);
       });
@@ -50,7 +50,7 @@ function App(): JSX.Element {
     return <Text style={styles.NoDatas}>No Userdatas</Text>;
   };
 
-  const renderItem = ({item}: {item: ItemProps}) => {
+  const renderItem = ({ item }: { item: ItemProps }) => {
     return <Child item={item} />;
   };
 
@@ -91,7 +91,7 @@ function App(): JSX.Element {
       </View>
       <View style={styles.contactList}>
         {refreshing ? (
-          <View style={styles.Loader}>
+          <View style={styles.LoaderItem}>
             <Text>Loading..</Text>
             <ActivityIndicator size="large" color="red" />
           </View>
@@ -99,7 +99,7 @@ function App(): JSX.Element {
           <FlatList
             data={data}
             renderItem={renderItem}
-            keyExtractor={item => item.email}
+            keyExtractor={(item) => item.email}
             refreshControl={
               <RefreshControl
                 refreshing={refreshing}
@@ -121,32 +121,38 @@ const styles = StyleSheet.create({
     fontSize: 20,
     marginTop: 20,
     marginBottom: 10,
-    textAlign: 'center',
-    fontWeight: 'bold',
-    color: 'black',
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "black",
   },
   endofPage: {
     marginTop: 10,
-    alignItems: 'center',
+    alignItems: "center",
     fontSize: 20,
-    textAlign: 'center',
+    textAlign: "center",
   },
   NoDatas: {
-    textAlign: 'center',
+    textAlign: "center",
     marginTop: 400,
     fontSize: 20,
-    fontWeight: 'bold',
+    fontWeight: "bold",
   },
   contactList: {
     paddingBottom: 150,
   },
+  LoaderItem:{
+    marginVertical: 16,
+    alignItems: "center",
+    marginTop:400,
+  },
   Loader: {
     marginVertical: 16,
-    alignItems: 'center',
+    alignItems: "center",
+    marginTop:400
   },
   HeaderContent: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    justifyContent: "space-between",
     paddingHorizontal: 20,
   },
 });
