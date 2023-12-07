e34ee85d32f5e323d8a37b47bd715a52069faa97
Hemanath Chakkaravarthi <114560429+hemanath1112@users.noreply.github.com>
2023-12-07 15:47:42 +0530

    updated

--- App.tsx
+++ App.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
-import {NavigationContainer} from '@react-navigation/native';
-import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
-import Home from './src/Screens/homePage';
-import UserList from './src/Screens/userList';
-import Camera from './src/Screens/cameraPage';
-import MapPage from './src/Screens/mapPage';
+import Home from "./src/Screens/homePage";
+import UserList from "./src/Screens/userList";
+import Camera from "./src/Screens/cameraPage";
+import MapPage from "./src/Screens/mapPage";
 
 const App = () => {
   const stack = createNativeStackNavigator();
@@ -16,15 +16,15 @@ const App = () => {
         <stack.Screen
           name="Home"
           component={Home}
-          options={{headerShown: false}}
+          options={{ headerShown: false }}
         />
         <stack.Screen
           name="userList"
           component={UserList}
-          options={{title: 'Users Details'}}
+          options={{ title: "Users Details" }}
         />
         <stack.Screen name="camera" component={Camera} />
-        <stack.Screen name='Location' component={MapPage} />
+        <stack.Screen name="Location" component={MapPage} />
       </stack.Navigator>
     </NavigationContainer>
   );
